bd6fa9cb22a4c34fc050dc6dbe3532830b1977a0
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-24 14:45:48 -0700

    Add files via upload

---
+++ app81.py
@@ -0,0 +1,23 @@
+import random
+import streamlit as st 
+num=0
+num1=random.randint(1,20)
+num2=random.randint(1,20)
+sign=random.choice(['+','-','*','/'])
+if sign== '+':
+  sc=num1+num2
+if sign== '-':
+  sc=num1-num2
+if sign== '*':
+  sc=num1*num2
+if sign== '/':
+  sc=num1/num2
+st.title("أهلاً بك في لعبه الذكاء التابعه لمحمد رياض ")
+st.write(num1,sign,num2)
+number=st.number_input("أدخل الناتج ")
+if st.button("تأكيد التخمين "):
+ if number==sc:
+  st.success("اجابتك صحيحه  ")
+ else:
+ st.error("اجابتك خطأ")
+ 
